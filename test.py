7e6202bf2585f684d5214833b14ff2f0280d3a9a
python <pansingh1203>
2026-05-07 11:37:28 +0000

    add a new function to pull all the video ids on all pages

--- test.py
+++ test.py
@@ -8,32 +8,8 @@ load_dotenv(dotenv_path="./.env")
 API_KEY =os.getenv("API_KEY")
 CHANNEL_HANDLE = "campusx-official"
 maxResults = 60
+playlist_id = "UUCWi3hpnq_Pe03nGxuS7isg"
 
-def get_playlist_id():
-    try:
-        url =  f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
-        
-        response = requests.get(url)
-
-        response.raise_for_status()
-
-        data1 = response.json()
-
-        data = json.dumps(data1, indent=4)
-
-        #print(data)
-
-        channel_items = data1["items"][0]
-
-        channel_playlist_id = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
-
-        print(channel_playlist_id)
-
-        return channel_playlist_id
-    
-    except requests.exceptions.RequestException as e:
-        raise e
-    
 def get_video_ids(playlist_id):
     video_ids = []
     pageToken = None
@@ -45,27 +21,30 @@ def get_video_ids(playlist_id):
             if pageToken:
                 url += f"&pageToken={pageToken}"
             response = requests.get(url)
+            print("pehla step poora")
             
-
             response.raise_for_status()
 
             data1 = response.json()
-            
-
+            print("dusra step poora")
+           
             for item in data1.get('items',[]):
-                
+                print("loop step shuru")
+                print(item)
                 video_id = item['contentDetails']['videoId']
+                print(video_id)
+                print(video_ids)
                 video_ids.append(video_id)
+                print(video_ids)
 
             pageToken = data1.get('nextPageToken')
-            
+               
             if not pageToken:
                 break
         return video_ids
 
     except requests.exceptions.RequestException as e:
         raise e
-
+    
 if __name__ == "__main__":
-    playlist_id = get_playlist_id()
     get_video_ids(playlist_id)
